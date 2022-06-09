38dca73882120af52ac3d201f21d43ae0bb460f5
Ondra Hlaváč <ondra@hlavac.cz>
2022-06-09 08:10:11 +0200

    feat: adds section dotace

--- app/pages/dotace.vue
+++ app/pages/dotace.vue
@@ -1,10 +1,291 @@
 <template>
   <section class="page">
-    <h1 class="page__title text-lg md:text-xl lg:text-4xl xl:text-6xl text-center py-8 md:py-16">Komplexní přístup k dotacím</h1>
-
-    <div class="page__content markdown pt-4 md:pt-6 md:pb-24">
-      Zaměřujeme se na celý proces dotačních možností, od designu projektových záměrů, přes vyhledání vhodného dotačního titulu, získání
-      dotace až po konzultace s poskytovateli dotací pro nastavení podmínek dotačních titulů.
+    <div class="py-12 bg-white">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+          <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">Dotace</h2>
+          <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Komplexní přístup k dotacím</p>
+          <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            V oblasti dotačního poradenství uplatňujeme komplexní a systémový přístup. Řešíme řízení portfolia projektů z pohledu možných
+            dotačních příležitostí.
+          </p>
+        </div>
+        <div class="mt-10">
+          <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div class="h-6 w-6" aria-hidden="true">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Náš přístup</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Zaměřujeme se na celý proces dotačních možností, od designu projektových záměrů, přes vyhledání vhodného dotačního titulu,
+                získání dotace až po konzultace s poskytovateli dotací pro nastavení podmínek dotačních titulů.
+              </dd>
+            </div>
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div class="h-6 w-6" aria-hidden="true">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M7 12l3-3 3 3 4-4M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Design projektových záměrů</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Zpracujeme design projektového záměru, na základě vize, myšlenky klienta. Zpracování projektového záměru probíhá v souladu
+                se strategií klienta, jeho dlouhodobých cílů a preferencí, které respektujeme.
+              </dd>
+            </div>
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div class="h-6 w-6" aria-hidden="true">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Analýza dotačních příležitostí</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Projekty porovnáváme s aktuálními možnostmi financování z národních a evropských fondů. Identifikujeme podmínky, které
+                projekty budou muset splňovat, aby měly na podporu nárok.
+              </dd>
+            </div>
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <svg
+                    class="h-6 w-6"
+                    x-description="Heroicon name: outline/scale"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                    ></path>
+                  </svg>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Komunikace s poskytovatelem a konzultace</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Nabízíme asistenci a konzultaci při vyjednávání s poskytovateli dotace o nastavení podmínek jednotlivých výzev.
+              </dd>
+            </div>
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div class="h-6 w-6" aria-hidden="true">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Dotační strategie</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Připravíme dotační strategii z pohledu času, finančních možností a kvality. Zaměřujeme se na monitoring dotačních
+                příležitostí a přípravu a prioritizaci projektů, které by mohly být podpořeny.
+              </dd>
+            </div>
+            <div class="relative">
+              <dt>
+                <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div class="h-6 w-6" aria-hidden="true">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Příprava žádostí</p>
+              </dt>
+              <dd class="mt-2 ml-16 text-base text-gray-500">
+                Nabízíme zajištění přípravy dotačních žádostí včetně příloh, investičního záměru, harmonogramu a CBA dle pravidel daných
+                výzev.
+              </dd>
+            </div>
+          </dl>
+          <!--          <div class="mt-8 mx-auto inline-flex rounded-md shadow">-->
+          <!--            <a-->
+          <!--              href="/kontakt"-->
+          <!--              class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"-->
+          <!--            >-->
+          <!--              Kontaktujte nás-->
+          <!--            </a>-->
+          <!--          </div>-->
+        </div>
+      </div>
+      <!-- druhá sekce -->
+      <div class="mt-20 mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+        <p class="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Tailor-made</p>
+        <p class="mx-auto mt-5 max-w-prose text-xl text-gray-500">
+          Dotační poradenství vnímáme komplexně jako soubor na sebe navazujících služeb. Ke každému klientovy přistupujeme individuálně a
+          prosazujeme Tailor&#8209;made&nbsp;přístup.
+        </p>
+        <div class="mt-12">
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <div class="pt-6">
+              <div class="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                      <div class="h-6 w-6 text-white" aria-hidden="true">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="2"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          />
+                        </svg>
+                      </div>
+                    </span>
+                  </div>
+                  <p class="mt-5 text-base text-gray-500">
+                    Máme zkušenosti s řízením velkých projektů v řádech desítek milionů korun. Pro řízení projektů používáme vlastní
+                    metodiku projektového řízení, která vychází z metodik Prince2 a IPMA.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="pt-6">
+              <div class="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                      <div class="h-6 w-6 text-white" aria-hidden="true">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="2"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                          />
+                        </svg>
+                      </div>
+                    </span>
+                  </div>
+                  <p class="mt-5 text-base text-gray-500">
+                    Během realizace a udržitelnosti projektu klademe důraz na dodržování podmínek pro čerpání dotace stanovených
+                    poskytovatelem dotace a legislativou ČR a EU.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="pt-6">
+              <div class="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+                <div class="-mt-6">
+                  <div>
+                    <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                      <div class="h-6 w-6 text-white" aria-hidden="true">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-6 w-6"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          stroke-width="2"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                          />
+                        </svg>
+                      </div>
+                    </span>
+                  </div>
+                  <p class="mt-5 text-base text-gray-500">
+                    Nabízíme asistenci a zajištění realizace výběrových řízení. Naše služby pokrývají celý postup výběrového řízení od
+                    spolupráce při návrhu zadávací dokumentace, přes hodnocení nabídek až po kontroly od poskytovatele dotace.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -51,7 +332,6 @@ export default class Dotace extends Vue {
     try {
       const page = require(`@/content/pages/dotace.json`);
 
-      console.log('ooo', page.content);
       return {
         page,
       };
