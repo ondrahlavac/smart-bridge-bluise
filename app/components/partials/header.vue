3af5c29b7a0e4a802d44f99b4dcd3b834310cb3f
Ondra Hlaváč <ondra@hlavac.cz>
2022-06-13 12:40:52 +0200

    feat: musíme to opravit

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -10,6 +10,7 @@
             </nuxt-link>
           </div>
           <div style="z-index: 101" @click="mobileMenuShow = true" @keydown.enter="mobileMenuShow = true" class="-mr-2 -my-2 md:hidden">
+            <br /><br /><br />
             <button
               class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             >
