92b0e13e1c4394ee6cf41ceb1ed8e909b442f457
Ondra Hlaváč <ondra@hlavac.cz>
2022-06-13 12:32:26 +0200

    feat: musíme to opravit

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -9,7 +9,7 @@
               <img class="h-8 w-auto sm:h-10" :src="logo" alt="SmartBridge" />
             </nuxt-link>
           </div>
-          <div @click="mobileMenuShow = true" @keydown.enter="mobileMenuShow = true" class="-mr-2 -my-2 md:hidden">
+          <div style="z-index: 101" @click="mobileMenuShow = true" @keydown.enter="mobileMenuShow = true" class="-mr-2 -my-2 md:hidden">
             <button
               class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             >
@@ -162,7 +162,6 @@
           </div>
         </transition>
       </div>
-      <br /><br /><br />
     </header>
   </section>
 </template>
