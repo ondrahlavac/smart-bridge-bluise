2a2abd11715ae87ea87952864c34503edf60116e
Ondra Hlaváč <ondra@hlavac.cz>
2022-06-13 09:08:07 +0200

    feat: testing menu

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -11,8 +11,8 @@
           </div>
           <div class="-mr-2 -my-2 md:hidden">
             <div
-              @click="mobileMenuShow = true"
-              @keydown.enter="mobileMenuShow = true"
+              @click="showMenu()"
+              @keydown.enter="showMenu()"
               class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             >
               <span class="sr-only">Otevřít nabídku</span>
