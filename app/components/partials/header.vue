615641a8026cb996ba1e022877ae7e57deca3aee
Ondra Hlaváč <ondra@hlavac.cz>
2022-06-13 12:49:37 +0200

    feat: musíme to opravit

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -3,14 +3,8 @@
     <header>
       <div class="relative bg-white">
         <div class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
-          <div class="flex justify-start lg:w-0 lg:flex-1">
-            <nuxt-link to="/">
-              <span class="sr-only">Smart Bridge</span>
-              <img class="h-8 w-auto sm:h-10" :src="logo" alt="SmartBridge" />
-            </nuxt-link>
-          </div>
+          <div class="flex justify-start lg:w-0 lg:flex-1"></div>
           <div style="z-index: 101" @click="mobileMenuShow = true" @keydown.enter="mobileMenuShow = true" class="-mr-2 -my-2 md:hidden">
-            <br /><br /><br />
             <button
               class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
             >
