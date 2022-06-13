0dc30b4a6e8961dd619360341aecb43d0cc6188c
Ondra Hlaváč <ondra@hlavac.cz>
2022-06-13 13:30:41 +0200

    feat: musíme to opravit

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -2,46 +2,47 @@
   <section>
     <header>
       <div class="relative bg-white">
-        <div class="relative z-20"></div>
-        <div class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
-          <div class="flex justify-start lg:w-0 lg:flex-1">
-            <nuxt-link to="/">
-              <span class="sr-only">Smart Bridge</span>
-              <img class="h-8 w-auto sm:h-10" :src="logo" alt="SmartBridge" />
-            </nuxt-link>
-          </div>
-          <div class="-mr-2 -my-2 md:hidden sm:pt-12">
-            <button
-              class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-            >
-              <span class="sr-only">Otevřít nabídku</span>
-              <svg
-                class="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                aria-hidden="true"
+        <div class="relative z-20">
+          <div class="flex justify-between items-center max-w-7xl mx-auto px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
+            <div class="flex justify-start lg:w-0 lg:flex-1">
+              <nuxt-link to="/">
+                <span class="sr-only">Smart Bridge</span>
+                <img class="h-8 w-auto sm:h-10" :src="logo" alt="SmartBridge" />
+              </nuxt-link>
+            </div>
+            <div class="-mr-2 -my-2 md:hidden sm:pt-12">
+              <button
+                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
               >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
-          </div>
-          <div class="hidden md:flex space-x-10">
-            <nuxt-link to="/" class="text-base font-medium text-gray-500 hover:text-gray-900"> O nás </nuxt-link>
-            <nuxt-link to="/iot" class="text-base font-medium text-gray-500 hover:text-gray-900"> Internet of Things </nuxt-link>
-            <nuxt-link to="/dotace" class="text-base font-medium text-gray-500 hover:text-gray-900"> Dotace </nuxt-link>
-          </div>
-          <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <nuxt-link
-              to="/kontakt"
-              class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >Kontakt
-            </nuxt-link>
+                <span class="sr-only">Otevřít nabídku</span>
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+              </button>
+            </div>
+            <div class="hidden md:flex space-x-10">
+              <nuxt-link to="/" class="text-base font-medium text-gray-500 hover:text-gray-900"> O nás </nuxt-link>
+              <nuxt-link to="/iot" class="text-base font-medium text-gray-500 hover:text-gray-900"> Internet of Things </nuxt-link>
+              <nuxt-link to="/dotace" class="text-base font-medium text-gray-500 hover:text-gray-900"> Dotace </nuxt-link>
+            </div>
+            <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+              <nuxt-link
+                to="/kontakt"
+                class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+                >Kontakt
+              </nuxt-link>
+            </div>
           </div>
         </div>
-</div>
+
         <transition
           enter-active-class="duration-200 ease-out"
           enter-from-class="opacity-0 scale-95"
